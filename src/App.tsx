9ed03f2626b6097e998e4f29e463a3f10e935db8
Togrul2001 <mammadov17878515@gmail.com>
2024-07-11 03:38:11 +0400

    routes and sidebar menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,58 @@
 import React from 'react';
 import './App.css';
+import { createBrowserRouter, Outlet, RouterProvider, useNavigate } from 'react-router-dom';
+import SidebarMenu from './components/SidebarMenu';
+
+function MainLayout() {
+  const navigate = useNavigate()
+  return (
+    <>
+      <main style={{width:"100vw", height:"100vh"}}>
+        <SidebarMenu />
+        <Outlet />
+      </main>
+    </>);
+}
+
+const routes = [
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <MainLayout />,
+        children:[
+          {
+            path: "lessons",
+            element: <>Lessons</>
+          },
+          {
+            path: "students",
+            element: <>Students</>
+          },
+          {
+            path: "exams",
+            element: <>Exams</>
+          },
+        ]
+      },
+
+
+    ],
+  },
+];
+
+function Root() {
+
+  return (
+    <Outlet />
+  );
+}
 
 function App() {
   return (
-    <>
-      Salam
-    </>
+    <RouterProvider router={createBrowserRouter(routes)} />
   );
 }
 
--- src/components/SidebarMenu/index.tsx
+++ src/components/SidebarMenu/index.tsx
@@ -1,1 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Drawer, Menu } from 'antd';
+import { RxHamburgerMenu } from "react-icons/rx";
+import { Link } from 'react-router-dom';
+import { PiStudentBold, PiBooksBold, PiExam } from "react-icons/pi";
+import { FaSignOutAlt } from "react-icons/fa";
+
+
+const SidebarMenu: React.FC = () => {
+
+    const items = [
+        {
+            key: "1",
+            icon: <PiStudentBold size={20}/>,
+            label: <Link to="students">Tələbələr</Link>,
+        },
+        {
+            key: "2",
+            icon: <PiBooksBold size={20}/>,
+            label: <Link to="lessons">Dərslər</Link>,
+        },
+        {
+            key: "3",
+            icon: <PiExam size={20}/>,
+            label: <Link to="exams">İmtahanlar</Link>,
+        },
+    ];
+    
+  const [open, setOpen] = useState(false);
+
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Button type="text" onClick={showDrawer} style={{ padding: "5px", display: "flex", alignItems: "center" }}>
+        <RxHamburgerMenu size={35}/>
+      </Button>
+      <Drawer title="Tədris Menu" onClose={onClose} open={open} placement='left'
+        bodyStyle={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '100%' }}
+      >
+        <Menu
+            defaultSelectedKeys={['1']}
+            mode="inline"
+            theme="light"
+            items={items}
+            style={{fontSize:"15px"}}
+        />
+        <Button
+          type="text"
+          style={{marginTop: 'auto', display: 'flex', alignItems: 'center', backgroundColor:"red", color:"white" }}
+        >
+          <FaSignOutAlt style={{ marginRight: 8 }} />
+          Çıxış
+        </Button>
+      </Drawer>
+    </>
+  );
+};
+
+export default SidebarMenu;
